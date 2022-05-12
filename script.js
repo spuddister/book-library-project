0f2d6ba35992fbfcdf45dc904abc6ee1a8fa12ae
Sam Puddister <sampuddister@gmail.com>
2022-05-11 23:12:00 -0400

    Built function for creating new HTML and adding the content of a book object.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const shelf = document.getElementsByClassName('columns')[0];
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -19,4 +20,71 @@ myLibrary[0] = book1;
 myLibrary[1] = book2;
 myLibrary[2] = book3;
 
-console.table(myLibrary);
+function repopLibrary () {
+    for (let book in myLibrary) {
+        addToShelf(book);
+    }
+}
+
+function addToShelf (book) {
+    //Build HTML components for new card - many div components due to Bulma Framework
+    let column = document.createElement('div');
+    let card = document.createElement('div');
+    let cardContent = document.createElement('div');
+    let content = document.createElement('div');
+    let title = document.createElement('p');
+    let author = document.createElement('p');
+    let pages = document.createElement('p');
+    let readStatus = document.createElement('p');
+    let timeAdded = document.createElement('p');
+    let cardFooter = document.createElement('footer');
+    let readBtn = document.createElement('a');
+    let deleteBtn = document.createElement('a');
+
+    //Append components together
+    column.appendChild(card);
+    card.appendChild(cardContent);
+    card.appendChild(cardFooter);
+    cardContent.appendChild(content);
+    content.appendChild(title);
+    content.appendChild(author);
+    content.appendChild(pages);
+    content.appendChild(readStatus);
+    content.appendChild(timeAdded);
+    cardFooter.appendChild(readBtn);
+    cardFooter.appendChild(deleteBtn);
+
+    //Add classes to apply Bulma properties
+    column.classList.add('column','is-one-third');
+    card.classList.add('card');
+    cardContent.classList.add('card-content');
+    content.classList.add('content');
+    title.classList.add('title','is-4');
+    author.classList.add('subtitle');
+    readStatus.classList.add('read-status','has-text-weight-bold','has-text-right');
+    timeAdded.classList.add('has-text-right','is-size-7');
+    cardFooter.classList.add('card-footer');
+    readBtn.classList.add('card-footer-item');
+    deleteBtn.classList.add('card-footer-item');
+
+    //Add content from book to new HTML card
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = 'Pages: ' + book.pages;
+    deleteBtn.textContent = 'Delete';
+    if(book.read) {
+        readStatus.textContent = 'Read';
+        readBtn.textContent = 'Mark as Unread';
+        readStatus.classList.add('has-text-success');
+    } else {
+        readStatus.textContent = 'Unread';
+        readBtn.textContent = 'Mark as Read';
+        readStatus.classList.add('has-text-danger');
+    }
+    const time = new Date();
+    timeAdded.textContent = 'Added: ' + time.toLocaleString();
+
+    shelf.appendChild(column);
+}
+
+addToShelf(myLibrary[2]);
